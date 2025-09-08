9877cc25892a7c94cf058d93ffe1b2fcdaf7043b
navinpatel58 <navin58@gmail.com>
2025-09-08 05:46:00 +0100

    Remove redundant code, duplicate hyperlinks, duplicate code and comments. Cleaned and renamed to index_v2.php.

---
+++ index_v2.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Bhagavata Dharma - Home Page</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="keywords" content="Dinadhyatma,Bhaktivedanta Manor lectures,bhagavatadharma,Bhagavad Gita,Srimad Bhagavatham, srimad bhagavatam lectures,bhagavatham,srila prabhupad,srila prabupada,Srila Prabhupada,Prabhu">
+<meta name="robots" content="index,follow">
+<link rel="icon" href="./images/om2.ico">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="build/mediaelement-and-player.min.js"></script>
+<script src="https://kit.fontawesome.com/e4c982d79f.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="build/mediaelementplayer.min.css" />
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="./css/navigation.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<style>
+body, h1, h2, h3, h4, h5 {font-family: "Raleway", sans-serif;}
+body {
+    font-family: Verdana, sans-serif;
+    background-color: #F3F3F3;
+    font-size: 14px;
+    color: #5F71FF;
+    margin: 0;
+    padding: 10px;
+}
+.center { display: block; margin-left: auto; margin-right: auto; width: 50%; }
+.mySlides {display: none;}
+img {vertical-align: middle;}
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+.text, .numbertext {
+  color: #f2f2f2;
+  position: absolute;
+  width: 100%;
+  text-align: center;
+}
+.text { font-size: 15px; padding: 8px 12px; bottom: 8px; }
+.numbertext { font-size: 12px; padding: 8px 12px; top: 0; }
+.dot, .dot1, .dot2, .dot3, .dot4 {
+  height: 15px;
+  width: 15px;
+  border-radius: 50%;
+  display: inline-block;
+  margin: 0 2px;
+}
+.dot { background-color: #396; transition: background-color 0.6s ease; }
+.active { background-color: #FF0000; }
+.dot1 { background-color: #F00; }
+.dot2 { background-color: #73AD21; }
+.dot3 { background-color: #F90; }
+.dot4 { background-color: #33F; }
+.fade { animation: fade 1.5s; }
+@keyframes fade { from {opacity:.4} to {opacity:1} }
+@media only screen and (max-width: 300px) { .text {font-size: 11px} }
+#playlist { list-style: none; }
+#playlist li a { color:black; text-decoration: none; }
+#playlist .current-song a { color:blue; }
+.blinking {
+  animation: opacity 5s ease-in-out infinite alternate;
+  opacity: 1;
+}
+@keyframes opacity {
+  0%, 10% { opacity: 0.1; }
+  25% { opacity: 0.25; }
+  50% { opacity: 0.5; }
+  75% { opacity: 0.75; }
+  90% { opacity: 0.9; }
+  100% { opacity: 1; }
+}
+.line_break {
+    width:800px;
+    height: 5px;
+    float: left;
+    background-color: rgba(255,255,255,.5);
+    border: none;
+    border-bottom: 3px solid black;
+}
+.lokesh { color:red; font-weight: 600; }
+br { position: relative; top: 20px; border: none; height: 12px; background: black; margin-bottom: 50px; }
+table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+}
+th, td {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: left;
+}
+th { background-color: #f2f2f2; }
+.download-box {
+    display: inline-block;
+    border: 1px solid #B3E0FF;
+    border-radius: 4px;
+    padding: 8px 16px;
+    margin: 8px 0;
+    background-color: #F5F9FF;
+}
+.tooltip {
+  position: relative;
+  display: inline-block;
+}
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 120px;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+  position: absolute;
+  z-index: 1;
+}
+.tooltip:hover .tooltiptext { visibility: visible; }
+audio {
+  transition:all 0.5s linear;
+  box-shadow: 2px 2px 4px 0px #006773;
+  border-radius:7px;
+}
+audio:hover, audio:focus, audio:active {
+  box-shadow: 15px 15px 20px rgba(0,0,0,0.4);
+  transform: scale(1.05);
+}
+</style>
+</head>
+<?php include("./include/idcount1.php"); ?>
+<body class="w3-light-grey">
+
+<div id="confirm" style="display:none;background-color:#FFF;color:#000;border:16px solid #C60;position:fixed;width:800px;height:600px;left:50%;margin-left:-550px;margin-bottom:300px;padding:10px 20px 10px;box-sizing:border-box;text-align:center;">
+  <div class="message"><br/><br/><font color="#3333FF"><center><font size="+3">"Please note"</font></center><br/><br/><center>23rd August 2020</center><br/><br/>"Hi, I am making changes to this website..."</font></div>
+  <br/><br/><br/><br/>
+  <button class="yes">OK</button>
+</div>
+
+<div class="w3-content" style="max-width:1400px">
+<header class="w3-container w3-center w3-padding-8">
+  <h1><b>Bhagavata Dharma </b></h1>
+  <p>Welcome to the Collection of Bhagavad Geeta &amp; Srimad Bhagavatham Audio website</p>
+</header>
+
+<div class="w3-row">
+<div class="w3-col l8 s12">
+  <div class="w3-card-4 w3-margin w3-white">
+    <div class="slideshow-container">
+      <?php
+      $images = [
+        "vrindavan-2.jpg", "vrindavan-3.jpg", "vrindavan-4.jpg", "vrindavan-5.jpg", "vrindavan-6.jpg",
+        "vrindavan-4.jpg", "vrindavan-8.jpg", "vrindavan-9.jpg", "vrindavan-10.jpg", "vrindavan-11.jpg", "vrindavan-12.jpg"
+      ];
+      foreach ($images as $i => $img) {
+        echo '<div class="mySlides fade"><div class="numbertext">' . ($i+1) . ' / 12</div><img src="images/' . $img . '" height="284" style="width:100%"><div class="text"></div></div>';
+      }
+      ?>
+    </div>
+    <br>
+    <div style="text-align:center">
+      <?php for ($i=0; $i<12; $i++) echo '<span class="dot"></span>'; ?>
+    </div>
+    <div class="w3-container">
+      <center>
+        <ul><strong>Satsang</strong> with <em>Dinadyathma Prabhu.</em></ul>
+      </center>
+      <?php include("./include/menu.php"); ?>
+    </div>
+    <p><center>
+      <em><font size="-1" color="#3366FF">New to this website or need help on how to make best use of this website?</font></em>
+      <em><font size="-2">Please click </font></em> - <a href="http://www.bhagavatadharma.co.uk/newbie.php"><font color="#FF0000" size="-2">here</font></a>
+      <p><font color="#040D50" size="-1">Do you want to keep up to date with Satsangam events? Please join our WhatsApp group "BhagavatamGroup" using the contact form.</font></p>
+      <p><span class="w3-badge w3-light-gray"></span>  <a href="http://www.bhagavatadharma.co.uk/statistics.php" target="_blank"><font color="#3366FF">Visitors Statistics</font></a></p>
+    </center></p>
+    <div class="w3-container">
+      <div class="w3-panel w3-card w3-pale-red">
+        <br>
+        <div class="w3-container w3-sand">
+          <div>
+            <center><font color="#EF070B"><h3>Regular Satsang 2025 Schedule</h3></font></center>
+            <table>
+              <tr>
+                <th style="width:50%">Date</th>
+                <th style="width:25%">Venue</th>
+                <th style="width:25%">Times</th>
+              </tr>
+              <tr><td>Every Wednesday (Bhagavad Gita)</td><td>Lokesh Prabhu Residence</td><td>4:00 am - 8:00 am</td></tr>
+              <tr><td>Every <font color="#B00837">Saturday</font> (Srimad Bhagavatham)</td><td>Lokesh Prabhu's Residence</td><td>7:00 am - 11:00 am</td></tr>
+              <tr><td>Every <font color="#B00837">Sunday</font> (Srimad Bhagavatham)</td><td>Letchmore Village Hall</td><td>7:00 am - 11:00 am</td></tr>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="w3-container w3-sand">
+        <div class="w3-col m6 w3-center">
+          <h4 class="w3-light-gray">Bhagavad Geeta</h4>
+          <p><a href="http://www.bhagavatadharma.co.uk/bhagavadgita_series1.php">Bhagavad Geeta <em><span class="w3-tag w3-pale-red">BG - Series 1</span></em></a></p>
+          <p><a href="http://www.bhagavatadharma.co.uk/bhagavadgita_series2.php">Bhagavad Geeta <em><span class="w3-tag w3-pale-red">BG - Series 2</span></em></a></p>
+          <p><a href="http://www.bhagavatadharma.co.uk/bhagavadgita_series3.php">Bhagavad Geeta <em><span class="w3-tag w3-pale-red">BG - Series 3</span></em></a></p>
+          <p><a href="http://www.bhagavatadharma.co.uk/bg_verses.php">Bhagavad Geeta -<span class="w3-tag w3-pale-green">Audio recitation</span></a></p>
+        </div>
+        <div class="w3-col m6 w3-left">
+          <h4 class="w3-light-gray">Srimad Bhagavatham</h4>
+          <p><a href="http://www.bhagavatadharma.co.uk/sb_main_page_series1.php">Srimad Bhagavatham <em><span class="w3-tag w3-pale-red">SB - Series 1</span></em></a></p>
+          <p><a href="http://www.bhagavatadharma.co.uk/sb_main_page_series2.php">Srimad Bhagavatham <em><span class="w3-tag w3-pale-red">SB - Series 2</span></em></a></p>
+          <p><a href="http://www.bhagavatadharma.co.uk/misc_lectures.php?Subject='Nimi_Nava_Yogi_Samvadam'">Nimi Nava Yogi Samvadam (Canto 11) <em><span class="w3-tag w3-pale-red">SB - Series 3</span></em></a></p>
+          <p><a href="http://www.bhagavatadharma.co.uk/main_canto_by_canto_lectures.php">SB Canto by Canto <em><span class="w3-tag w3-pale-red">SB - Series 4</span></em></a></p>
+          <p><a href="http://www.bhagavatadharma.co.uk/Thrikkazhippuram.php">Audio recitation by Thrikkazhippuram</a></p>
+          <p><a href="http://www.bhagavatadharma.co.uk/perumballi_2.php">Audio recitation by Perumballi <font size="-2" color="#F10918">Clear your cache first (F5)</font></a></p>
+          <p><a href="http://www.bhagavatadharma.co.uk/sbsanskrit_verses_main.php">Audio recitation by Mrs Manda Kulkarni</a></p>
+          <p><a href="./seven_day_programme/seven_day_programme.html">Srimad Bhagavatham - Audio recitation - UNDER CONSTRUCTION</a></p>
+          <p><a href="http://www.bhagavatadharma.co.uk/devotees_prayers.php">Srimad Bhagavatham - Devotees Stutis</a></p>
+        </div>
+      </div>
+      <div class="w3-card w3-sand">
+        <?php $cnt = 1; ?>
+        <p><font size="-1" color="#2212E9">Current satsangham is on Shridhar Swami.</font></p>
+        <div class="w3-animate-right">
+          <p><span class="w3-badge w3-light-gray"><?php echo $cnt++; ?>.</span> <a href="http://www.bhagavatadharma.co.uk/shridhar_swami_main.php">Shridhar Swami commentary on Srimad Bhagavatham</a> <span class="blinking"><font color="#E916E2"><b>Current Satsangham...</b></font></span></p>
+          <p><span class="w3-badge w3-light-gray"><?php echo $cnt++; ?>.</span> <a href="http://www.bhagavatadharma.co.uk/brahmopadesam.php">Brahmopadesam (includes 2024 Janmashtami audios)</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div> <!-- END w3-content -->
+
+<?php include("./include/footer.php"); ?>
+
+<script src="https://code.jquery.com/jquery-2.2.0.js"></script>
+<script src="./audioplayerpl/audioPlayer.js"></script>
+<script>
+function myFunction(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+</script>
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("May 22, 2021 14:00:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("Count_Down").innerHTML = days + " days - " + hours + " hours - " + minutes + " mins - " + seconds + " secs ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("Count_Down").innerHTML = "Classes now Started!";
+  }
+}, 1000);
+</script>
+</body>
+</html>
